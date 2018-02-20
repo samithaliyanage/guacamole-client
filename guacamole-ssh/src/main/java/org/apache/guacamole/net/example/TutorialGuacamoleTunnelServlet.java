73ac995c4379a3ac9a6db4d775f0017a562c1b6a
Samitha Liyanage <samitha.liyanage@gmail.com>
2018-02-20 11:25:41 -0500

    Added guacamole ssh removed sample private key

--- guacamole-ssh/src/main/java/org/apache/guacamole/net/example/TutorialGuacamoleTunnelServlet.java
+++ guacamole-ssh/src/main/java/org/apache/guacamole/net/example/TutorialGuacamoleTunnelServlet.java
@@ -21,21 +21,7 @@ public class TutorialGuacamoleTunnelServlet
     config.setParameter("port", "16032");
     config.setParameter("username", "dcuser");
     //config.setParameter("password", "dcuser");
-    config.setParameter("private-key", "-----BEGIN RSA PRIVATE KEY-----\n" +
-        "MIICXAIBAAKBgQDKDkrbngEtytdqVwaMbQLoiC+EV6Zmlf4pQ8rRCGy4AB3Rv8h+\n" +
-        "6Fpkr8EGY1Zo65+BHf8H5sU5TK7D4Y4d+fZf9N48d1Celb4NzTh8Zup6rBIZUmvx\n" +
-        "9eYOqTp09r35P9HC+Z1klryCoNYbvnzWuIG4fTdhODi777sH4KX/F0GVtQIDAQAB\n" +
-        "AoGASwCJKwJ4v43G7pNFPAUPCk/bC44Zl4Tf5mNiursc2CQ67Wv6OZl4TF2tEfj3\n" +
-        "z1R/Tcdbrowr83wSd2WXgmsR151qaiXrqcmHs89B8X/DHWXjxJAFq7Z8i3WgRRkC\n" +
-        "goYLkpeKH1KtqWgUlzZXIyCl6uyAuCFaSTIVlF8wr7m8d/UCQQDzX8+xkvsVyOM1\n" +
-        "ywAHXhfXRsdd0DqOrITE8Yq6nDxiEvRI361UZMD1Ct9WdI4CjCaawjOXV58ZHYGB\n" +
-        "rwOFdNb/AkEA1Im99MASl5Z8GvX5tWwmy7tfYqs7BC1HknrJqZFMzvLWnP3rfQKh\n" +
-        "/Ph2Q548ZppI7BcdS6hKqdHvw5vfenpnSwJAf2Kz9G9JayKmAp8JmdGfSvfqHGmE\n" +
-        "bO9R4z/GbFygonjnUkl5kQXXlasmMTt6pUy0XCawGFQtm1i9U6LGhhEdAwJBAMOo\n" +
-        "ekLSf0slseJwBw1JHv325HzE/XN+66ChgPylUnxBeejUqPFQdyeW4+ijxx79cZxG\n" +
-        "OyXnK7TiG0OMP1NVU7kCQFPoObaIPx+4hQ4SMEybKPxBHgn2kzVU3BjhpmyMXSeA\n" +
-        "TUY/DI5pyCTbUVvg3E2/IrESD0ppfHecAh9xWVFhZ+8=\n" +
-        "-----END RSA PRIVATE KEY-----");
+    config.setParameter("private-key", "<PRIVATE_KEY_VALUE>");
 
 
     // Connect to guacd - everything is hard-coded here.
